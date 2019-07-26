46bcab57544f75e36579601c9721b4cd3fa45020
daverona <egkimatwork@gmail.com>
2019-07-26 12:56:53 +0900

    Add cppcheck

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['cpplint==1.4.4']
+    install_requires=[
+        'cpplint==1.4.4',
+        'cppcheck==1.88',
+    ],
 )
